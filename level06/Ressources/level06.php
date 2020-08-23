ea74cf294bf766bbf4424d520703f51ca7ec8a62
Samuel BELONDRADE <sbelondr@student.42.fr>
2020-08-23 02:36:42 +0200

    trop rapide pour toi

--- level06/Ressources/level06.php
+++ level06/Ressources/level06.php
@@ -1,6 +1,23 @@
 #!/usr/bin/php
 <?php
-function y($m) { $m = preg_replace("/\./", " x ", $m); $m = preg_replace("/@/", " y", $m); return $m; }
-function x($y, $z) { $a = file_get_contents($y); $a = preg_replace("/(\[x (.*)\])/e", "y(\"\\2\")", $a); $a = preg_replace("/\[/", "(", $a); $a = preg_replace("/\]/", ")", $a); return $a; }
-$r = x($argv[1], $argv[2]); print $r;
+function y($m) {
+	$m = preg_replace("/\./", " x ", $m);
+	$m = preg_replace("/@/", " y", $m);
+	return $m;
+}
+
+function x($y, $z) {
+	// ouverture du fichier et stocker dans a
+	$a = file_get_contents($y);
+	// /e can be exploited: <h1>{${eval($_GET[php_code])}}</h1>
+	$a = preg_replace("/(\[x (.*)\])/e", "y(\"\\2\")", $a);
+	$a = preg_replace("/\[/", "(", $a);
+	$a = preg_replace("/\]/", ")", $a);
+	return $a;
+}
+
+$r = x($argv[1], $argv[2]);
+
+print $r;
+
 ?>
